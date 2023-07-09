73820247d633e533d0b5a01bb4ad757fe99b70fa
Allahverdi Asgarli <nuraibaa@code.edu.az>
2023-07-09 16:56:01 +0400

    Create Menu and SubMenu

---
+++ Market_System/Market_System/Program.cs
@@ -0,0 +1,47 @@
+﻿using Market_System.Services;
+using Market_System.SubMenu;
+
+namespace Market_System
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int option;
+
+            do
+            {
+                Console.WriteLine("1. Operate on Product");
+                Console.WriteLine("2. Operate on Sales");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("-----------");
+                Console.WriteLine("Enter option:");
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid number!");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine("Enter option:");
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        SubMenuHelper.ProductSubMenu();
+                        break;
+                    case 2:
+                        SubMenuHelper.SaleSubMenu();
+                        break;
+                    case 0:
+                        Console.WriteLine("Bye!");
+                        break;
+                    default:
+                        Console.WriteLine("No such option!");
+                        break;
+                }
+            } while (option != 0);
+        }
+    }
+}
---
+++ Market_System/Market_System/Services/MenuServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_System.Services
+{
+    public class MenuServices
+    {
+        private static MarketService marketService = new();
+
+    }
+}
